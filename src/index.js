0fc9c2144228c9217dc9f2954191a38c8893a9f8
msukalinski <msukalinski@gmail.com>
2026-06-23 20:52:38 +0300

    add-breed and add-cat pages

--- src/index.js
+++ src/index.js
@@ -2,11 +2,35 @@ import http from 'http';
 import fs from 'fs/promises';
 
 const server = http.createServer(async (req, res) => {
-    const homePage = await fs.readFile('./src/views/home/index.html', 'utf-8');
+    if (req.url.startsWith('/styles')) {
+        const cssContent = await fs.readFile('./src/styles/site.css', 'utf-8');
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.write(cssContent);
+        return res.end();
+    }
 
-    res.writeHead(200, { 'content-type': 'text/html' });
-    res.write(homePage);
+    if (req.url === '/js/script.js') {
+        const jsContent = await fs.readFile('./src/js/script.js', 'utf-8');
 
+        res.writeHead(200, { 'Content-Type': 'text/css' });
+        res.write(jsContent);
+        return res.end();
+    }
+
+    let htmlContent = '';
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    if (req.url === '/') {
+        htmlContent = await fs.readFile('./src/views/home/index.html', 'utf-8');
+    } else if (req.url === '/cats/add-breed') {
+        htmlContent = await fs.readFile('./src/views/addBreed.html', 'utf-8');
+    } else if (req.url === '/cats/add-cat') {
+        htmlContent = await fs.readFile('./src/views/addCat.html', 'utf-8');
+    } else {
+        htmlContent = await fs.readFile('./src/views/notFound.html');
+    }
+
+    res.write(htmlContent);
     res.end();
 });
 
