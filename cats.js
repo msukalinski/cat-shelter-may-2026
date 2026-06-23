5b631c8b1b2bf5a55be21d1cbc44bf12c0db7511
msukalinski <msukalinski@gmail.com>
2026-06-23 21:49:31 +0300

    show cats dynamically based on array

---
+++ cats.js
@@ -0,0 +1,49 @@
+const cats = [
+    {
+        id: 1,
+        name: 'Navcho',
+        imageUrl: "https://t3.ftcdn.net/jpg/02/36/99/22/360_F_236992283_sNOxCVQeFLd5pdqaKGh8DRGMZy7P4XKm.jpg",
+        breed: "Bombay Cat",
+        description:
+            "Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.",
+        price: null
+    },
+    {
+        id: 2,
+        name: "Sisa",
+        imageUrl: "https://cdn.pixabay.com/photo/2015/06/19/14/20/cat-814952_1280.jpg",
+        breed: "Bombay Cat",
+        description:
+            "Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.",
+        price: null
+    },
+    {
+        id: 3,
+        name: "Garry",
+        imageUrl: "https://cdn.pixabay.com/photo/2018/08/08/05/12/cat-3591348_1280.jpg",
+        breed: "Bombay Cat",
+        description:
+            "Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.",
+        price: null
+    },
+    {
+        id: 4,
+        name: "Mishy",
+        imageUrl: "https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg",
+        breed: "Bombay Cat",
+        description:
+            "Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.",
+        price: 350
+    },
+    {
+        id: 5,
+        name: "Misty",
+        imageUrl: "https://cdn.pixabay.com/photo/2014/04/13/20/49/cat-323262_1280.jpg",
+        breed: "Bombay Cat",
+        description:
+            "Dominant and aggressive to other cats. Will probably eat you in your sleep. Very cute tho.",
+        price: null
+    }
+];
+
+export default cats;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import http from 'http';
 import fs from 'fs/promises';
+import cats from '../cats.js';
 
 const server = http.createServer(async (req, res) => {
     if (req.url.startsWith('/styles')) {
@@ -21,17 +22,41 @@ const server = http.createServer(async (req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
 
     if (req.url === '/') {
-        htmlContent = await fs.readFile('./src/views/home/index.html', 'utf-8');
+        htmlContent = await renderHomePage();
     } else if (req.url === '/cats/add-breed') {
         htmlContent = await fs.readFile('./src/views/addBreed.html', 'utf-8');
     } else if (req.url === '/cats/add-cat') {
         htmlContent = await fs.readFile('./src/views/addCat.html', 'utf-8');
     } else {
-        htmlContent = await fs.readFile('./src/views/notFound.html');
+        htmlContent = await fs.readFile('./src/views/notFound.html', 'utf-8');
     }
 
     res.write(htmlContent);
     res.end();
 });
 
+async function renderHomePage() {
+    let htmlContent = await fs.readFile('./src/views/home/index.html', 'utf-8');
+
+    const catTemplate = (cat) => `
+        <li>
+            <img src="${cat.imageUrl}" alt="${cat.name}">
+            <h3>${cat.name}</h3>
+            <p><span>Breed: </span>${cat.breed}</p>
+            <p><span>Description: </span>${cat.description}</p>
+            <ul class="buttons">
+                <li class="btn edit"><a href="">Change Info</a></li>
+                <li class="btn delete"><a href="">New Home</a></li>
+            </ul>
+        </li>`;
+
+    const catsContent = `<ul>${cats.map(cat => catTemplate(cat)).join('\n')}</ul>`;
+
+    const result = htmlContent.replace('{{cats}}', catsContent);
+
+    return result;
+}
+
 server.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
+
+show cats dynamically 
