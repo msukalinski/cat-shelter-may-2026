762ecb501ccae3caed4c61d03048946cb5088145
msukalinski <msukalinski@gmail.com>
2026-06-24 22:53:02 +0300

    show search word after search

--- src/index.js
+++ src/index.js
@@ -120,7 +120,8 @@ async function renderHomePage(filter = {}) {
 
     const catsContent = `<ul>${cats.map(cat => catTemplate(cat)).join('\n')}</ul>`;
 
-    const result = htmlContent.replace('{{cats}}', catsContent);
+    const result = htmlContent.replace('{{cats}}', catsContent)
+        .replace('{{name}}', filter.name || '');
 
     return result;
 }
